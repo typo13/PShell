997b73ea659238286d5ccf0748ff59d2b5234736
typo13 <typo13@gmail.com>
2016-10-05 12:06:44 -0400

    Moved to backup

--- Change DNS/DNS.ps1
+++
@@ -1,8 +0,0 @@
-$computer = get-content C:\sl.txt
-$NICs = Get-WMIObject Win32_NetworkAdapterConfiguration -computername $computer |where{$_.IPEnabled -eq “TRUE”}
-  Foreach($NIC in $NICs) {
-
-$DNSServers = “172.16.100.24",”172.16.100.3"
- $NIC.SetDNSServerSearchOrder($DNSServers)
- $NIC.SetDynamicDNSRegistration(“TRUE”)
-}
